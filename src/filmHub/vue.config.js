0d5888f609e7e7a9a249c297209c19fc12cd8ed7
Veroski <guillemvera@gmail.com>
2024-10-27 18:35:14 +0100

    Register backend and frontend connected

--- src/filmHub/vue.config.js
+++ src/filmHub/vue.config.js
@@ -2,3 +2,10 @@ const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true
 })
+
+// vue.config.js
+module.exports = {
+  devServer: {
+    proxy: 'http://127.0.0.1:8000', // redirige las solicitudes al backend
+  },
+};
