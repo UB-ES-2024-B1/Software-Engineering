db0c10b71df11205d71e98343397e8b5816a834b
Mattyete <90678148+Mattyete@users.noreply.github.com>
2024-11-19 01:39:49 +0100

    Revert "Us 3.1"

--- src/filmHub/src/router/index.js
+++ src/filmHub/src/router/index.js
@@ -2,7 +2,6 @@ import { createRouter, createWebHistory } from 'vue-router';
 import MainPageView from '../views/MainPageView.vue';
 import UserRegister from '../views/UserRegister.vue';
 import UserLogin from '../views/UserLogin.vue'; // Importa el componente de login
-import AllMovies from '@/views/AllMovies.vue';
 
 const routes = [
   {
@@ -19,11 +18,6 @@ const routes = [
     path: '/login', // Nueva ruta para el login
     name: 'UserLogin',
     component: UserLogin,
-  },
-  {
-    path: '/movies', 
-    name: 'AllMovies',
-    component: AllMovies,
   },
 ];
 
