a8b0737cb6801b3244fe41bfedbbb39221177001
Veroski <guillemvera@gmail.com>
2024-11-19 18:09:11 +0100

    aareeglado

--- src/filmHub/src/main.js
+++ src/filmHub/src/main.js
@@ -5,7 +5,7 @@ import 'bootstrap';
 import { createApp } from 'vue';
 import App from './App.vue';
 import router from './router/index'; // Asegúrate de que la ruta es correcta
-import './firebase';
+import '../firebase';
 
 const app = createApp(App);
 app.use(router);
