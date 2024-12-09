a7eaeb44b8164e8d69e13680940c9fb370339295
RakGH <rnunezpa11@alumnes.ub.edu>
2024-12-09 13:04:37 +0100

    End to end tests wished movie

--- src/backend/app/tests/end_to_end/wish_movie_test.py
+++ src/backend/app/tests/end_to_end/wish_movie_test.py
@@ -17,6 +17,7 @@ def driver_setup():
     chrome_options = Options()
     chrome_options.add_argument("--disable-gpu")
     chrome_options.add_argument("--disable-dev-shm-usage")
+    chrome_options.add_argument("--headless")
     driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=chrome_options)
     driver.maximize_window()
     driver.get("http://localhost:8080/login")
@@ -40,93 +41,99 @@ def login_user(driver_setup, db_session):
     time.sleep(3)  # Wait for the page to load after login
     return driver
 
-def test_like_movie_success(login_user, db_session):
+def test_wish_movie_success(login_user, db_session):
     driver = login_user
     # Navigate to the movie page
     driver.get("http://localhost:8080/movie/2")  # Change this URL to the movie page URL
     time.sleep(3)
 
-    WebDriverWait(driver, 20).until(EC.presence_of_element_located((By.ID, "like-toggle")))  # Wait for the liking option
-    
+    # Wait for the element to be clickable
     WebDriverWait(driver, 20).until(
-        EC.element_to_be_clickable((By.XPATH, "//div[@class='checkmark']"))
+        EC.element_to_be_clickable((By.XPATH, "//label[@class='ui-bookmark wishlist-button']/div[@class='bookmark']"))
     )
-    
-    # Locate the div with the checkmark using XPath
-    checkmark_div = driver.find_element(By.XPATH, "//div[@class='checkmark']")
-    
-    
-    # Click the checkbox to toggle like
-    checkmark_div.click()
+
+    # Locate the element
+    bookmark_div = driver.find_element(By.XPATH, "//label[@class='ui-bookmark wishlist-button']/div[@class='bookmark']")
+
+    time.sleep(1)  # Optional: Let the page settle after scrolling
+
+    # Now click the element
+    bookmark_div.click()
     time.sleep(3)
+
+    # Check if the checkbox inside the label is selected 
+    checkbox = driver.find_element(By.XPATH, "//label[@class='ui-bookmark wishlist-button']/input[@type='checkbox']")
+    assert checkbox.is_selected(), "The wishlist state is not selected after the click."
     
-    # Assert the checkbox is selected after clicking
-    assert driver.find_element(By.ID, "like-toggle").is_selected(), "Liking was not saved correctly."
-
     user = user_crud.get_user_by_email(db_session, "user2@example.com")
-    movies = movie_crud.get_user_liked_movies(db_session, user.id)
+    movies = movie_crud.get_user_wished_movies(db_session, user.id)
     movie = movie_crud.get_movie(db_session, 2)
-    assert any(m == movie.title for m in movies), f"Movie '{movie.title}' is not in the liked movies list."
+    assert any(m == movie.title for m in movies), f"Movie '{movie.title}' is not in the wish movies list."
 
-    movie_crud.remove_movie_like(db_session, 2, user.id)
+    movie_crud.remove_wish_movie(db_session, user.id, 2)
 
-def test_non_like_movie_success(login_user, db_session):
+def test_non_wish_movie_success(login_user, db_session):
     driver = login_user
     # Navigate to the movie page
     driver.get("http://localhost:8080/movie/3")  # Change this URL to the movie page URL
     time.sleep(3)
 
-    WebDriverWait(driver, 20).until(EC.presence_of_element_located((By.ID, "like-toggle")))  # Wait for the liking option
-    
+    # Wait for the element to be clickable
     WebDriverWait(driver, 20).until(
-        EC.element_to_be_clickable((By.XPATH, "//div[@class='checkmark']"))
+        EC.element_to_be_clickable((By.XPATH, "//label[@class='ui-bookmark wishlist-button']/div[@class='bookmark']"))
     )
-    
-    # Locate the div with the checkmark using XPath
-    checkmark_div = driver.find_element(By.XPATH, "//div[@class='checkmark']")
-    
-    
-    # Click the checkbox to toggle like
-    checkmark_div.click()
+
+    # Locate the element
+    bookmark_div = driver.find_element(By.XPATH, "//label[@class='ui-bookmark wishlist-button']/div[@class='bookmark']")
+
+    time.sleep(1)  # Optional: Let the page settle after scrolling
+
+    # Now click the element
+    bookmark_div.click()
     time.sleep(3)
+
+    # Check if the checkbox inside the label is selected 
+    checkbox = driver.find_element(By.XPATH, "//label[@class='ui-bookmark wishlist-button']/input[@type='checkbox']")
+    assert checkbox.is_selected(), "The wishlist state is not selected after the click."
     
-    # Assert the checkbox is selected after clicking
-    assert driver.find_element(By.ID, "like-toggle").is_selected(), "Liking was not saved correctly."
-    
-    # Click the checkbox to toggle like
-    checkmark_div.click()
+    # Now click the element
+    bookmark_div.click()
     time.sleep(3)
-    
-    # Assert the checkbox is not selected after clicking
-    assert not driver.find_element(By.ID, "like-toggle").is_selected(), "Liking was not saved correctly."
+
+    # Check if the checkbox inside the label is selected
+    checkbox = driver.find_element(By.XPATH, "//label[@class='ui-bookmark wishlist-button']/input[@type='checkbox']")
+    assert not checkbox.is_selected(), "The wishlist state is not selected after the click."
 
     user = user_crud.get_user_by_email(db_session, "user2@example.com")
-    movies = movie_crud.get_user_liked_movies(db_session, user.id)
+    movies = movie_crud.get_user_wished_movies(db_session, user.id)
     movie = movie_crud.get_movie(db_session, 3)
-    assert all(m != movie.title for m in movies), f"Movie '{movie.title}' is unexpectedly in the liked movies list."
+    assert all(m != movie.title for m in movies), f"Movie '{movie.title}' is unexpectedly in the wish movies list."
 
-def test_liking_logged_out_user(driver_setup, db_session):
+def test_wishing_logged_out_user(driver_setup, db_session):
     driver = driver_setup
     # Navigate to the movie page without logging in
     driver.get("http://localhost:8080/movie/3")
 
-    WebDriverWait(driver, 20).until(EC.presence_of_element_located((By.ID, "like-toggle")))  # Wait for the liking option
     
+    # Wait for the element to be clickable
     WebDriverWait(driver, 20).until(
-        EC.element_to_be_clickable((By.XPATH, "//div[@class='checkmark']"))
+        EC.element_to_be_clickable((By.XPATH, "//label[@class='ui-bookmark wishlist-button']/div[@class='bookmark']"))
     )
-    
-    # Locate the div with the checkmark using XPath
-    checkmark_div = driver.find_element(By.XPATH, "//div[@class='checkmark']")
-    
-    # Click the checkbox to toggle like
-    checkmark_div.click()
 
+    # Locate the element
+    bookmark_div = driver.find_element(By.XPATH, "//label[@class='ui-bookmark wishlist-button']/div[@class='bookmark']")
+
+    time.sleep(1)  # Optional: Let the page settle after scrolling
+
+    # Now click the element
+    bookmark_div.click()
+    time.sleep(3)
+   
     # Handle the alert and check the text
     WebDriverWait(driver, 20).until(EC.alert_is_present())  # Wait for alert to appear
     alert = driver.switch_to.alert  # Switch to the alert
     alert_text = alert.text  # Get the alert text
-    assert alert_text == "You must log in to give like a movie.", f"Unexpected alert text: {alert_text}"
+    assert alert_text == "You must log in to add to your wishlist.", f"Unexpected alert text: {alert_text}"
     alert.accept()  # Close the alert
 
     # Verify the user was redirected to the login page
@@ -135,29 +142,35 @@ def test_liking_logged_out_user(driver_setup, db_session):
     # Verify that the user was redirected to the login page
     assert "login" in driver.current_url, "User was not redirected to login page."
 
+
 def test_check_list_actualization(login_user, db_session):
     driver = login_user
     driver.get("http://localhost:8080/movie/6")
 
-    WebDriverWait(driver, 20).until(EC.presence_of_element_located((By.ID, "like-toggle")))  # Wait for the liking option
     
+    # Wait for the element to be clickable
     WebDriverWait(driver, 20).until(
-        EC.element_to_be_clickable((By.XPATH, "//div[@class='checkmark']"))
+        EC.element_to_be_clickable((By.XPATH, "//label[@class='ui-bookmark wishlist-button']/div[@class='bookmark']"))
     )
-    
-    # Locate the div with the checkmark using XPath
-    checkmark_div = driver.find_element(By.XPATH, "//div[@class='checkmark']")
-    
-    # Click the checkbox to toggle like
-    checkmark_div.click()
 
-    time.sleep(2)
-    assert driver.find_element(By.ID, "like-toggle").is_selected(), "Liking was not saved correctly."
+    # Locate the element
+    bookmark_div = driver.find_element(By.XPATH, "//label[@class='ui-bookmark wishlist-button']/div[@class='bookmark']")
+
+    time.sleep(1)  # Optional: Let the page settle after scrolling
+
+    # Now click the element
+    bookmark_div.click()
+    time.sleep(3)
+
+    # Check if the checkbox inside the label is selected 
+    checkbox = driver.find_element(By.XPATH, "//label[@class='ui-bookmark wishlist-button']/input[@type='checkbox']")
+    assert checkbox.is_selected(), "The wishlist state is not selected after the click."
+    
 
     user = user_crud.get_user_by_email(db_session, "user2@example.com")
-    movies = movie_crud.get_user_liked_movies(db_session, user.id)
+    movies = movie_crud.get_user_wished_movies(db_session, user.id)
     movie = movie_crud.get_movie(db_session, 6)
-    assert any(m == movie.title for m in movies), f"Movie '{movie.title}' is not in the liked movies list."
+    assert any(m == movie.title for m in movies), f"Movie '{movie.title}' is not in the wished movies list."
 
     driver.get("http://localhost:8080/profile")
     time.sleep(2)
@@ -170,10 +183,10 @@ def test_check_list_actualization(login_user, db_session):
         EC.presence_of_element_located((By.CLASS_NAME, "profile-page"))
     )
 
-    # Click on the "Favourite Movies" button if not active
-    liked_button = driver.find_element(By.XPATH, "//button[contains(text(), 'Favourite Movies')]")
-    if "active" not in liked_button.get_attribute("class"):
-        liked_button.click()
+    # Click on the "Wishlist Movies" button if not active
+    wish_button = driver.find_element(By.XPATH, "//button[contains(text(), 'Wishlist Movies')]")
+    if "active" not in wish_button.get_attribute("class"):
+        wish_button.click()
 
     # Wait for the movies list to load
     WebDriverWait(driver, 20).until(
@@ -190,11 +203,11 @@ def test_check_list_actualization(login_user, db_session):
     )
 
     time.sleep(3) 
-    # Check if the movie with id=1 is in the liked list
+    # Check if the movie with id=1 is in the wished list
     movie_item = driver.find_element(By.XPATH, "//div[@class='movie-item']//a[contains(@href, '/movie/6')]")
     ActionChains(driver).move_to_element(movie_item).perform()
-    assert movie_item.is_displayed(), "Movie with id=6 is not displayed in the Favourite Movies list."
+    assert movie_item.is_displayed(), "Movie with id=6 is not displayed in the Wishlist Movies list."
     
     time.sleep(3) 
 
-    movie_crud.remove_movie_like(db_session, 6, user.id)
+    movie_crud.remove_wish_movie(db_session, user.id, 6)
