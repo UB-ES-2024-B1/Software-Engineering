b905f3fbc2939cb39b70870ee13508d17da4a2d2
Veroski <guillemvera@gmail.com>
2024-12-10 20:49:50 +0100

    casi funcional followers backend

--- src/backend/app/crud/user_crud.py
+++ src/backend/app/crud/user_crud.py
@@ -111,10 +111,37 @@ def update_user_by_email(db: Session, email: str, user_data: dict):
     
     return user
 
-def get_followers(db: Session, user_email: str) -> list:
-    # Get followers by user email
-    return db.query(User).join(Follow, Follow.followed_email == user_email).all()
+def get_followers(db: Session, user_id: int) -> list:
+    """
+    Get followers for a user.
+    """
+    return db.query(User).join(Follow, Follow.follower_id == User.id).filter(Follow.followed_id == user_id).all()
 
-def get_followed_users(db: Session, user_email: str) -> list:
-    # Get followed users by user email
-    return db.query(User).join(Follow, Follow.follower_email == user_email).all()
+def get_followed_users(db: Session, user_id: int) -> list:
+    """
+    Get followed users for a user.
+    """
+    return db.query(User).join(Follow, Follow.followed_id == User.id).filter(Follow.follower_id == user_id).all()
+
+
+# Función para seguir a un usuario
+def follow_user(db: Session, follower_id: int, followed_id: int) -> Follow:
+    # Verificar si ya existe un seguimiento
+    existing_follow = db.query(Follow).filter(Follow.follower_id == follower_id, Follow.followed_id == followed_id).first()
+    if existing_follow:
+        return existing_follow  # Si ya existe, devolverlo en lugar de crear uno nuevo
+    else:
+        follow = Follow(follower_id=follower_id, followed_id=followed_id)
+        db.add(follow)
+        db.commit()
+        db.refresh(follow)
+        return follow
+
+# Función para dejar de seguir a un usuario
+def unfollow_user(db: Session, follower_id: int, followed_id: int) -> bool:
+    follow = db.query(Follow).filter(Follow.follower_id == follower_id, Follow.followed_id == followed_id).first()
+    if follow:
+        db.delete(follow)
+        db.commit()
+        return True
+    return False
